de04dd00d24031e88a4b4ea8f2b9a89e6df73dc2
Marc DUROCHER <15232340+marcdurocher@users.noreply.github.com>
2018-12-24 00:43:36 +0100

    endpoint with a main() entrypoint creation of a test module

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # serial-python
-Experiments around Python functions serialization.
+
+##Experiments around Python functions serialization.
+
 
 __Endpoint.py__ exposes a function as "REST RPC" service.
 (Listening on port 8080 from the localhost currently)
@@ -9,3 +11,21 @@ The function attached to this HTTP endpoint is loaded from a serialized form, fo
 The goal is to decouple the creation of the function and its consumation as a "REST API".
 
 This function could be a trained model seralized form a machine learning framework.  
+
+##Example
+
+Service launch
+> python -m modele.endpoint --file estpair.bin
+
+where estpair.bin is the path to the file containing the serialized function estPair
+
+Service call
+> wget -qO - http://localhost:8080/api/13
+
+Result
+> {"number": 13, "estPair": false}
+
+
+Function __estpair__
+> def estpair(n):  return n % 2 == 0
+ 
--- modele/endpoint.py
+++ modele/endpoint.py
@@ -1,4 +1,7 @@
 import dill
+import getopt
+import sys
+
 from bottle import route, run
 
 
@@ -12,15 +15,41 @@ def estpair(number):
     rv = {"number": number, "estPair": _estpair(number)}
     return rv
 
-
 #
 # Importation de la fonction a partir du fichier "binaire"
 #
 # remarque: si le fichier estpair.bin n'est pas present il faut
 # le generer en executant le programme Python fonction_predictive
 #
-with open('../estpair.bin', 'rb') as file:
-    _estpair = dill.load(file)
 
 
-run(host='localhost', port=8080, debug=True)
+def main():
+    """
+    Fonction principale
+    :return: code retour
+    """
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], None, ['file='])
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(-1)
+
+    nomfichier = None
+
+    for o, a in opts:
+        if o == "--file":
+            nomfichier = a
+
+    if not nomfichier:
+            print("Nom de fichier attendu avec l'option --file <nom fichier>")
+            sys.exit(-2)
+
+    with open(nomfichier, 'rb') as file:
+        global _estpair
+        _estpair = dill.load(file)
+
+    run(host='localhost', port=8080, debug=True)
+
+
+if __name__ == "__main__":
+    main()
